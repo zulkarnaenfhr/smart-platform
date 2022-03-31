553c2bf0973f2ca1b489db1c106a315e53002999
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-03-31 11:28:19 +0700

    first commit

---
+++ src/Dashboard-BSP.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class DashboardBsp extends Component {
+    render() {
+        return (
+            <div>
+                <h1>masuk dashboard bs</h1>
+            </div>
+        );
+    }
+}
+
+export default DashboardBsp;
---
+++ src/container/1. Smart-Platform/Smart-Platform-LandingPage/SmartPlatformLandingPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Smartplatformlandingpage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>masuk smart platform landing page</h1>
+            </div>
+        );
+    }
+}
+
+export default Smartplatformlandingpage;
