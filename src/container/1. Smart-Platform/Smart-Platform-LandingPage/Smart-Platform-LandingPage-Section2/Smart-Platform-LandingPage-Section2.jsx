643f87d1f4d9ffba5afa67d07bb84f8e06cb48c8
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-04-03 11:25:41 +0700

    insyaallah SPL Landing Page Section 2 Done

--- src/container/1. Smart-Platform/Smart-Platform-LandingPage/Smart-Platform-LandingPage-Section2/Smart-Platform-LandingPage-Section2.jsx
+++ src/container/1. Smart-Platform/Smart-Platform-LandingPage/Smart-Platform-LandingPage-Section2/Smart-Platform-LandingPage-Section2.jsx
@@ -16,27 +16,6 @@ class SmartPlatformLandingpageSection2 extends Component {
                     <div className="SPL-LandingPage-Section2-Card-Container row">
                         <div className="col-4">
                             <Cardspllandingpage
-                                logoImg={`https://i.ibb.cob/4ZKX2SL/Section-2-Big-Score.png`}
-                                productName={`Telco Risk Scoring `}
-                                productDesc={`adalah angka yang memberikan informasi atau kategori Profil Risiko Telkom pelanggan yang mencerminkan seberapa baik atau buruk risiko kredit yang menurut pemberi pinjaman tertentu Anda.`}
-                                linkProduct={`notSet`}
-                            />
-                        </div>
-                        <div className="col-4">
-                            <Cardspllandingpage logoImg={`https://i.ibb.co/ZJWNHsp/Section-2-Big-Box.png`} productName={`Big Box `} productDesc={`lorem40`} linkProduct={`notSet`} />{" "}
-                        </div>
-                        <div className="col-4">
-                            <Cardspllandingpage
-                                logoImg={`https://i.ibb.co/wR4kQDX/Section-2-Smart-Eye.png`}
-                                productName={`Smarteye.id `}
-                                productDesc={`wujudkan imajinasi anda menjadi kenyataan bersama kami, dengan teknologi virtual dan augmented reality. Beragam produk dan layanan VR dan AR untuk berbagai kebutuhan anda.`}
-                                linkProduct={`notSet`}
-                            />{" "}
-                        </div>
-                    </div>
-                    <div className="SPL-LandingPage-Section2-Card-Container row">
-                        <div className="col-4">
-                            <Cardspllandingpage
                                 logoImg={`https://i.ibb.co/gvxh6zL/Section-2-Legal-Analytic.png`}
                                 productName={`Legal Analytics `}
                                 productDesc={`Merupakan aplikasi berbasis Big Data yang menghimpun seluruh peraturan perundang-undangan tersistem yang akurat, valid, dan up-to-date sehingga memudahkan pengguna`}
@@ -51,11 +30,33 @@ class SmartPlatformLandingpageSection2 extends Component {
                                 linkProduct={`notSet`}
                             />{" "}
                         </div>
+
                         <div className="col-4">
                             <Cardspllandingpage
                                 logoImg={`https://i.ibb.co/9gVjVR5/Section-2-Picaso.png`}
                                 productName={`Picaso `}
                                 productDesc={`Merupakan layanan API membantu para developer dan perusahaan dalam implementasi teknologi AI. Tiga fitur sesuai dengan kebutuhan Anda seperti Optical Character Recognition, Face Recognition, dan Object Detection.`}
+                                linkProduct={`notSet`}
+                            />{" "}
+                        </div>
+                    </div>
+                    <div className="SPL-LandingPage-Section2-Card-Container row">
+                        <div className="col-4">
+                            <Cardspllandingpage
+                                logoImg={`https://i.ibb.co/4ZKX2SL/Section-2-Big-Score.png`}
+                                productName={`Telco Risk Scoring `}
+                                productDesc={`adalah angka yang memberikan informasi atau kategori Profil Risiko Telkom pelanggan yang mencerminkan seberapa baik atau buruk risiko kredit yang menurut pemberi pinjaman tertentu Anda.`}
+                                linkProduct={`notSet`}
+                            />
+                        </div>
+                        <div className="col-4">
+                            <Cardspllandingpage logoImg={`https://i.ibb.co/ZJWNHsp/Section-2-Big-Box.png`} productName={`Big Box `} productDesc={`lorem40`} linkProduct={`notSet`} />{" "}
+                        </div>
+                        <div className="col-4">
+                            <Cardspllandingpage
+                                logoImg={`https://i.ibb.co/wR4kQDX/Section-2-Smart-Eye.png`}
+                                productName={`Smarteye.id `}
+                                productDesc={`wujudkan imajinasi anda menjadi kenyataan bersama kami, dengan teknologi virtual dan augmented reality. Beragam produk dan layanan VR dan AR untuk berbagai kebutuhan anda.`}
                                 linkProduct={`notSet`}
                             />{" "}
                         </div>
