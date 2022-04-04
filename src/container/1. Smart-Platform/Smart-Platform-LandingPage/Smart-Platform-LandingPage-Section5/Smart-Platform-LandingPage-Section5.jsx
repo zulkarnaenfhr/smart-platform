c630fa45d85193454a2c5c29f13023ab134bb404
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-04-04 16:06:55 +0700

    SPL Landing Page Section 5 Done

--- src/container/1. Smart-Platform/Smart-Platform-LandingPage/Smart-Platform-LandingPage-Section5/Smart-Platform-LandingPage-Section5.jsx
+++ src/container/1. Smart-Platform/Smart-Platform-LandingPage/Smart-Platform-LandingPage-Section5/Smart-Platform-LandingPage-Section5.jsx
@@ -27,6 +27,33 @@ class SmartPlatformLandingpageSection5 extends Component {
                         <img className="SPL-LandingPage-Section5-Content-CostSaving2-Logo" src="https://i.ibb.co/Tk1NF1n/Section-5-Cost-Saving2.png" alt="Section-5-Cost-Saving2" border="0"></img>
                         <img className="SPL-LandingPage-Section5-Content-CostSaving3-Logo" src="https://i.ibb.co/Jv7Bm19/Section-5-Cost-Saving3.png" alt="Section-5-Cost-Saving3" border="0"></img>
                     </div>
+                    <div
+                        className="SPL-LandingPage-Section5-Content-CostSaving-Title-Container"
+                        style={{
+                            display: "flex",
+                        }}
+                    >
+                        <div>
+                            <p className="SPL-LandingPage-Section5-Content-CostSaving1-Title">Cost-saving practices</p>
+                            <h3 className="SPL-LandingPage-Section5-Content-CostSaving1-Desc">Meningkatkan efisiensi biaya operasional untuk kebutuhan bisnis anda.</h3>
+                        </div>
+                        <div
+                            style={{
+                                marginLeft: "45px",
+                            }}
+                        >
+                            <p className="SPL-LandingPage-Section5-Content-CostSaving1-Title">Cost-saving practices</p>
+                            <h3 className="SPL-LandingPage-Section5-Content-CostSaving1-Desc">Meningkatkan efisiensi biaya operasional untuk kebutuhan bisnis anda.</h3>
+                        </div>
+                        <div
+                            style={{
+                                marginLeft: "45px",
+                            }}
+                        >
+                            <p className="SPL-LandingPage-Section5-Content-CostSaving1-Title">Cost-saving practices</p>
+                            <h3 className="SPL-LandingPage-Section5-Content-CostSaving1-Desc">Meningkatkan efisiensi biaya operasional untuk kebutuhan bisnis anda.</h3>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
