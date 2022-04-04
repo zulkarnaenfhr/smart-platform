39c1829f5ee3792f4fbbcd7c5f4068088203c0ca
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-04-04 12:51:34 +0700

    SPL Landing page Section 4 Scroll Header

--- src/container/1. Smart-Platform/Smart-Platform-LandingPage/Smart-Platform-LandingPage-Section4/Smart-Platform-LandingPage-Section4.jsx
+++ src/container/1. Smart-Platform/Smart-Platform-LandingPage/Smart-Platform-LandingPage-Section4/Smart-Platform-LandingPage-Section4.jsx
@@ -5,7 +5,20 @@ class SmartPlatformLandingpageSection4 extends Component {
     render() {
         return (
             <div id="SPL-LandingPage-Section4">
-                <h1>masuk section 4</h1>
+                <div className="SPL-LandingPage-Section4-Scroll-Container">
+                    <div className="SPL-LandingPage-Section4-Scroll-Header-Container">
+                        <div className="SPL-LandingPage-Section4-Scroll-Header-Tittle">
+                            What we’re offering Services{" "}
+                            <span
+                                style={{
+                                    color: "#D23D3D",
+                                }}
+                            >
+                                we’re providing to our clients
+                            </span>
+                        </div>
+                    </div>
+                </div>
                 <img className="SPL-LandingPage-Section4-Background" src="https://i.ibb.co/ncDFSH9/Section-3-Background.png" alt="Section-3-Background" border="0"></img>
             </div>
         );
