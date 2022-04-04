d67dee7a71d781320224e5a7bafdbe519a3af2e0
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-04-04 14:29:41 +0700

    add scroll card

---
+++ src/components/Card/SPL Landing Page/ScrollCardSPLLandingPage.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./ScrollCardSPLLandingPage.css";
+
+class Scrollcardspllandingpage extends Component {
+    render() {
+        return (
+            <div id="ScrollCardSPLLandingPage">
+                <img className="ScrollCardSPLLandingPage-logo" src={this.props.logo} alt="" border="0"></img>
+                <div className="ScrollCardSPLLandingPage-Text-Container">
+                    <h1>
+                        {this.props.judul}
+                    </h1>
+                    <p>
+                        {this.props.desc}
+                    </p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Scrollcardspllandingpage;
--- src/container/1. Smart-Platform/Smart-Platform-LandingPage/Smart-Platform-LandingPage-Section4/Smart-Platform-LandingPage-Section4.jsx
+++ src/container/1. Smart-Platform/Smart-Platform-LandingPage/Smart-Platform-LandingPage-Section4/Smart-Platform-LandingPage-Section4.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Scrollcardspllandingpage from "../../../../components/Card/SPL Landing Page/ScrollCardSPLLandingPage";
 import "./Smart-Platform-LandingPage-Section4.css";
 
 class SmartPlatformLandingpageSection4 extends Component {
@@ -7,7 +8,7 @@ class SmartPlatformLandingpageSection4 extends Component {
             <div id="SPL-LandingPage-Section4">
                 <div className="SPL-LandingPage-Section4-Scroll-Container">
                     <div className="SPL-LandingPage-Section4-Scroll-Header-Container">
-                        <div className="SPL-LandingPage-Section4-Scroll-Header-Tittle">
+                        <div className="SPL-LandingPage-Section4-Scroll-Header-Title">
                             What we’re offering Services{" "}
                             <span
                                 style={{
@@ -17,6 +18,33 @@ class SmartPlatformLandingpageSection4 extends Component {
                                 we’re providing to our clients
                             </span>
                         </div>
+                    </div>
+                    <div className="SPL-LandingPage-Section4-Scroll-Content-Container">
+                        <Scrollcardspllandingpage
+                            logo={`https://i.ibb.co/QpXwRBz/Section-4-Scroll-Card-Face-Detection.png`}
+                            judul={`Face Detection`}
+                            desc={`Dengan memanfaatkan teknologi 4.0 dalam mengelola dan menganalisa data dengan tepat, bisa menjadi peluang untuk melahirkan sebuah informasi yang tepat guna dan tepat sasaran.`}
+                        />
+                        <Scrollcardspllandingpage
+                            logo={`https://i.ibb.co/d0xvS6Z/Section-4-Scroll-Card-Document-Detection.png`}
+                            judul={`Document Detection`}
+                            desc={`Dengan memanfaatkan teknologi 4.0 dalam mengelola dan menganalisa data dengan tepat, bisa menjadi peluang untuk melahirkan sebuah informasi yang tepat guna dan tepat sasaran.`}
+                        />
+                        <Scrollcardspllandingpage
+                            logo={`https://i.ibb.co/gdT5S0r/Section-4-Scroll-Card-Data-Integration.png`}
+                            judul={`Data Integration`}
+                            desc={`Dengan memanfaatkan teknologi 4.0 dalam mengelola dan menganalisa data dengan tepat, bisa menjadi peluang untuk melahirkan sebuah informasi yang tepat guna dan tepat sasaran.`}
+                        />
+                        <Scrollcardspllandingpage
+                            logo={`https://i.ibb.co/n3LbKf4/Section-4-Scroll-Card-Payment-Behavior.png`}
+                            judul={`Payment Behavior`}
+                            desc={`Dengan memanfaatkan teknologi 4.0 dalam mengelola dan menganalisa data dengan tepat, bisa menjadi peluang untuk melahirkan sebuah informasi yang tepat guna dan tepat sasaran.`}
+                        />
+                        <Scrollcardspllandingpage
+                            logo={`https://i.ibb.co/XCSrvX6/Section-4-Scroll-Card-VR-Tour.png`}
+                            judul={`VR Tour`}
+                            desc={`Dengan memanfaatkan teknologi 4.0 dalam mengelola dan menganalisa data dengan tepat, bisa menjadi peluang untuk melahirkan sebuah informasi yang tepat guna dan tepat sasaran.`}
+                        />
                     </div>
                 </div>
                 <img className="SPL-LandingPage-Section4-Background" src="https://i.ibb.co/ncDFSH9/Section-3-Background.png" alt="Section-3-Background" border="0"></img>
