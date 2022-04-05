ef89a45f5fe5c18315c670cfa5ed74b84adbf990
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-04-05 09:47:04 +0700

    add file structure SPL LandingPage Section 6

---
+++ src/container/1. Smart-Platform/Smart-Platform-LandingPage/Smart-Platform-LandingPage-Section6/Smart-Platform-LandingPage-Section6.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Smart-Platform-LandingPage-Section6.css";
+
+class SmartPlatformLandingpageSection6 extends Component {
+    render() {
+        return (
+            <div>
+                <h1>masuk</h1>
+            </div>
+        );
+    }
+}
+
+export default SmartPlatformLandingpageSection6;
