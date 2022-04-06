8f393d8ebb45b7d349424573f72ddcea923073b5
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-04-06 13:11:25 +0700

    SPL Landing Page Offline File Vector Section 5 Done

--- src/container/1. Smart-Platform/Smart-Platform-LandingPage/Smart-Platform-LandingPage-Section5/Smart-Platform-LandingPage-Section5.jsx
+++ src/container/1. Smart-Platform/Smart-Platform-LandingPage/Smart-Platform-LandingPage-Section5/Smart-Platform-LandingPage-Section5.jsx
@@ -14,9 +14,9 @@ class SmartPlatformLandingpageSection5 extends Component {
                             display: "flex",
                         }}
                     >
-                        <img className="SPL-LandingPage-Section5-Content-CostSaving-Logo" src="https://i.ibb.co/w66NKnz/Section-5-Cost-Saving.png" alt="Section-5-Cost-Saving" border="0"></img>
-                        <img className="SPL-LandingPage-Section5-Content-CostSaving2-Logo" src="https://i.ibb.co/Tk1NF1n/Section-5-Cost-Saving2.png" alt="Section-5-Cost-Saving2" border="0"></img>
-                        <img className="SPL-LandingPage-Section5-Content-CostSaving3-Logo" src="https://i.ibb.co/Jv7Bm19/Section-5-Cost-Saving3.png" alt="Section-5-Cost-Saving3" border="0"></img>
+                        <img loading="lazy" className="SPL-LandingPage-Section5-Content-CostSaving-Logo" src={require("../../../../src/Smart-Platform/Landing Page/Section-5-Left.png")} alt="" border="0"></img>
+                        <img loading="lazy" className="SPL-LandingPage-Section5-Content-CostSaving2-Logo" src={require("../../../../src/Smart-Platform/Landing Page/Section-5-Middle.png")} alt="" border="0"></img>
+                        <img loading="lazy" className="SPL-LandingPage-Section5-Content-CostSaving3-Logo" src={require("../../../../src/Smart-Platform/Landing Page/Section-5-Right.png")} alt="" border="0"></img>
                     </div>
                     <div
                         className="SPL-LandingPage-Section5-Content-CostSaving-Title-Container"
