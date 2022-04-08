0b1ac568af802daefe22ce724a7a0bac371dbd71
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-04-08 08:37:21 +0700

    Update File Structure Navbar 1

--- src/components/Navbar/Smart platform/NavbarSPL.jsx
+++ src/components/Navbar/Smart platform/NavbarSPL.jsx
@@ -5,174 +5,189 @@ import { Link } from "react-router-dom";
 class Navbarspl extends Component {
     render() {
         return (
-            <div id="navbarSpl" data-aos="fade-up" data-aos-offset="200" data-aos-delay="0">
-                <img className="navbarLogo" src={require("../../../src/Smart-Platform/Landing Page/Logo-Navbar.png")} alt="" />
-                <div className="navbarMenu-container">
-                    <div className="navbarMenu dropdown">
-                        <button className="btn navbarMenu navbarMenuProduct dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Product
-                        </button>
-                        <div className="dropdown-menu dropdown-menu-container" aria-labelledby="dropdownMenuButton1">
-                            <table>
-                                <tbody>
-                                    <tr className="tr1">
-                                        <td className="td1">
+            <nav id="navbarSpl" class="navbar navbarSpl navbar-expand-lg navbar-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img className="SPLNavbarLogo" src={require("../../../src/Smart-Platform/Landing Page/Logo-Navbar.png")} alt="" />
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav SplnavbarMenu-container">
+                            <li class="nav-item dropdown">
+                                <a class="dropdown-toggle SplNavbarMenu" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Product
+                                </a>
+                                <ul class="dropdown-menu SplNavbarDropdownProduct-Container" aria-labelledby="navbarDropdown">
+                                    <div className="row">
+                                        <div className="col-6">
                                             <Link
-                                                className="Dropdown-Menu"
+                                                className="SplDropdown-Menu-Product"
                                                 to={{
                                                     pathname: `notset`,
                                                 }}
                                             >
                                                 <p>Picasso</p>
                                             </Link>
-                                        </td>
-                                        <td>
+                                        </div>
+                                        <div className="col-6">
                                             <Link
-                                                className="Dropdown-Menu"
+                                                className="SplDropdown-Menu-Product"
                                                 to={{
                                                     pathname: `notset`,
                                                 }}
                                             >
                                                 <p>Big View</p>
                                             </Link>
-                                        </td>
-                                    </tr>
-                                    <tr className="tr2">
-                                        <td>
+                                        </div>
+                                    </div>
+                                    <div className="row SplNavbarDropdownProduct-row2">
+                                        <div className="col-6">
                                             <Link
-                                                className="Dropdown-Menu"
+                                                className="SplDropdown-Menu-Product"
                                                 to={{
                                                     pathname: `notset`,
                                                 }}
                                             >
                                                 <p>Smart Eye</p>
                                             </Link>
-                                        </td>
-                                        <td>
+                                        </div>
+                                        <div className="col-6">
                                             <Link
-                                                className="Dropdown-Menu"
+                                                className="SplDropdown-Menu-Product"
                                                 to={{
                                                     pathname: `notset`,
                                                 }}
                                             >
                                                 <p>Big Box</p>
                                             </Link>
-                                        </td>
-                                    </tr>
-                                    <tr className="tr3">
-                                        <td>
+                                        </div>
+                                    </div>
+                                    <div className="row SplNavbarDropdownProduct-row3">
+                                        <div className="col-6">
                                             <Link
-                                                className="Dropdown-Menu"
+                                                className="SplDropdown-Menu-Product"
                                                 to={{
                                                     pathname: `notset`,
                                                 }}
                                             >
-                                                <p>Legal Analitycs</p>
+                                                <p>Legal Analytycs</p>
                                             </Link>
-                                        </td>
-                                        <td>
+                                        </div>
+                                        <div className="col-6">
                                             <Link
-                                                className="Dropdown-Menu"
+                                                className="SplDropdown-Menu-Product"
                                                 to={{
                                                     pathname: `notset`,
                                                 }}
                                             >
                                                 <p>Big Score</p>
                                             </Link>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    <Link
-                        className="navbarMenu navbarMenuSolution"
-                        to={{
-                            pathname: `notset`,
-                        }}
-                    >
-                        <p>Solution</p>
-                    </Link>
-                    <Link
-                        className="navbarMenu"
-                        to={{
-                            pathname: `notset`,
-                        }}
-                    >
-                        <p>Partnership</p>
-                    </Link>
-                    <div className="navbarMenu dropdown navbarMenuResources">
-                        <button className="btn navbarMenu  dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Resources
-                        </button>
-                        <div className="dropdown-menu dropdown-menu-container" aria-labelledby="dropdownMenuButton1">
-                            <div>
+                                        </div>
+                                    </div>
+                                </ul>
+                            </li>
+                            <li class="nav-item SplNavbarMenuSolution">
                                 <Link
-                                    className="Dropdown-Menu"
+                                    className="SplNavbarMenu"
                                     to={{
                                         pathname: `notset`,
                                     }}
                                 >
-                                    <p>Documentation</p>
+                                    <p>Solution</p>
                                 </Link>
+                            </li>
+                            <li class="nav-item SplNavbarMenuPartnership">
                                 <Link
-                                    className="Dropdown-Menu FAQ"
+                                    className="SplNavbarMenu"
                                     to={{
                                         pathname: `notset`,
                                     }}
                                 >
-                                    <p>FAQ</p>
+                                    <p>Partnership</p>
                                 </Link>
-                                <Link
-                                    className="Dropdown-Menu Support"
-                                    to={{
-                                        pathname: `notset`,
-                                    }}
-                                >
-                                    <p>Support</p>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="navbarMenu dropdown navbarMenuCompany">
-                        <button className="btn navbarMenu dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Company
-                        </button>
-                        <div className="dropdown-menu dropdown-menu-container" aria-labelledby="dropdownMenuButton1">
-                            <div>
-                                <Link
-                                    className="Dropdown-Menu"
-                                    to={{
-                                        pathname: `notset`,
-                                    }}
-                                >
-                                    <p>About SPL</p>
-                                </Link>
-                                <Link
-                                    className="Dropdown-Menu Blog"
-                                    to={{
-                                        pathname: `notset`,
-                                    }}
-                                >
-                                    <p>Blog</p>
-                                </Link>
-                                <Link
-                                    className="Dropdown-Menu Press"
-                                    to={{
-                                        pathname: `notset`,
-                                    }}
-                                >
-                                    <p>Press</p>
-                                </Link>
-                            </div>
-                        </div>
+                            </li>
+                            <li class="nav-item dropdown SplNavbarMenuResources">
+                                <a class="dropdown-toggle SplNavbarMenu" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Resources
+                                </a>
+                                <ul class="dropdown-menu SplNavbarDropdownResources-Container" aria-labelledby="navbarDropdown ">
+                                    <div>
+                                        <Link
+                                            className="SplDropdown-Menu-Recources"
+                                            to={{
+                                                pathname: `notset`,
+                                            }}
+                                        >
+                                            <p>Documentation</p>
+                                        </Link>
+                                        <div className="SplDropdown-Menu-Recources-row2">
+                                            <Link
+                                                className="SplDropdown-Menu-Recources"
+                                                to={{
+                                                    pathname: `notset`,
+                                                }}
+                                            >
+                                                <p>FAQ</p>
+                                            </Link>
+                                        </div>
+                                        <div className="SplDropdown-Menu-Recources-row3">
+                                            <Link
+                                                className="SplDropdown-Menu-Recources"
+                                                to={{
+                                                    pathname: `notset`,
+                                                }}
+                                            >
+                                                <p>Support</p>
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown SplNavbarMenuCompany">
+                                <a class="dropdown-toggle SplNavbarMenu" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Company
+                                </a>
+                                <ul class="dropdown-menu SplNavbarDropdownCompany-Container" aria-labelledby="navbarDropdown">
+                                    <div>
+                                        <Link
+                                            className="SplDropdown-Menu-Company"
+                                            to={{
+                                                pathname: `notset`,
+                                            }}
+                                        >
+                                            <p>About SPL</p>
+                                        </Link>
+                                        <div className="SplDropdown-Menu-Company-row2">
+                                            <Link
+                                                className="SplDropdown-Menu-Company"
+                                                to={{
+                                                    pathname: `notset`,
+                                                }}
+                                            >
+                                                <p>Blog</p>
+                                            </Link>
+                                        </div>
+                                        <div className="SplDropdown-Menu-Company-row3">
+                                            <Link
+                                                className="SplDropdown-Menu-Company"
+                                                to={{
+                                                    pathname: `notset`,
+                                                }}
+                                            >
+                                                <p>Press</p>
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </ul>
+                            </li>
+                            <button className="SplNavbarButtonDemoApps">DEMO APPS</button>
+                            <button className="SPlNavbarButtonContactUs">CONTACT US</button>
+                        </ul>
                     </div>
                 </div>
-                <div className="navbarButton-container">
-                    <button className="navbarButton buttonDemo">DEMO APPS</button>
-                    <button className="navbarButton navbarContactUs">CONTACT US</button>
-                </div>
-            </div>
+            </nav>
         );
     }
 }
--- src/container/1. Smart-Platform/Smart-Platform-LandingPage/SmartPlatformLandingPage.jsx
+++ src/container/1. Smart-Platform/Smart-Platform-LandingPage/SmartPlatformLandingPage.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Footerspl from "../../../components/Footer/Smart-Platform/FooterSPL";
-import Navbar from "../../../components/Navbar/Smart platform/Navbar";
 import Navbarspl from "../../../components/Navbar/Smart platform/NavbarSPL";
 import SmartPlatformLandingpageSection1 from "./Smart-Platform-LandingPage-Section1/Smart-Platform-LandingPage-Section1";
 import SmartPlatformLandingpageSection2 from "./Smart-Platform-LandingPage-Section2/Smart-Platform-LandingPage-Section2";
@@ -18,8 +17,7 @@ class Smartplatformlandingpage extends Component {
         return (
             <div>
                 <div className="SPL-LandingPage-navbar-container">
-                    {/* <Navbarspl colorFreeTrial={`white`} backgroundFreeTrial={`#D23D3D`} colorContactUs={`white`} backgroundContactUs={`#D23D3D`} />{" "} */}
-                    <Navbar />
+                    <Navbarspl />
                 </div>
                 <div className="SPL-LandingPage-content-container">
                     <SmartPlatformLandingpageSection1 />
