aa16ed7119a225a10ab570872cdd02dd8a399e2d
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-04-08 10:05:03 +0700

    SPL LandingPage Section 5 Done Responsive Desktop

--- src/container/1. Smart-Platform/Smart-Platform-LandingPage/Smart-Platform-LandingPage-Section5/Smart-Platform-LandingPage-Section5.jsx
+++ src/container/1. Smart-Platform/Smart-Platform-LandingPage/Smart-Platform-LandingPage-Section5/Smart-Platform-LandingPage-Section5.jsx
@@ -5,39 +5,41 @@ class SmartPlatformLandingpageSection5 extends Component {
     render() {
         return (
             <div id="SPL-LandingPage-Section5">
-                <div className="SPL-LandingPage-Section5-Content-Container" data-aos="fade-up" data-aos-offset="0">
-                    <h1 data-aos="fade-up" data-aos-offset="100">
-                        product achievement
-                    </h1>
-                    <div className="SPL-LandingPage-Section5-Content-Vector-Container">
-                        <img loading="lazy" className="SPL-LandingPage-Section5-Content-CostSaving-Logo" src={require("../../../../src/Smart-Platform/Landing Page/Section-5-Left.png")} alt="" border="0"></img>
-                        <img loading="lazy" className="SPL-LandingPage-Section5-Content-CostSaving2-Logo" src={require("../../../../src/Smart-Platform/Landing Page/Section-5-Middle.png")} alt="" border="0"></img>
-                        <img loading="lazy" className="SPL-LandingPage-Section5-Content-CostSaving3-Logo" src={require("../../../../src/Smart-Platform/Landing Page/Section-5-Right.png")} alt="" border="0"></img>
-                    </div>
-                    <div className="SPL-LandingPage-Section5-Content-CostSaving-Title-Container">
-                        <div>
-                            <p className="SPL-LandingPage-Section5-Content-CostSaving1-Title" data-aos="fade-up" data-aos-offset="100">
-                                Cost-saving practices
-                            </p>
-                            <h3 className="SPL-LandingPage-Section5-Content-CostSaving1-Desc" data-aos="fade-up" data-aos-offset="100">
-                                Meningkatkan efisiensi biaya operasional untuk kebutuhan bisnis anda.
-                            </h3>
+                <div className="SPL-LandingPage-Section5-Content-Container-Responsive">
+                    <div className="SPL-LandingPage-Section5-Content-Container" data-aos="fade-up" data-aos-offset="0">
+                        <h1 data-aos="fade-up" data-aos-offset="100">
+                            product achievement
+                        </h1>
+                        <div className="SPL-LandingPage-Section5-Content-Vector-Container">
+                            <img loading="lazy" className="SPL-LandingPage-Section5-Content-CostSaving-Logo" src={require("../../../../src/Smart-Platform/Landing Page/Section-5-Left.png")} alt="" border="0"></img>
+                            <img loading="lazy" className="SPL-LandingPage-Section5-Content-CostSaving2-Logo" src={require("../../../../src/Smart-Platform/Landing Page/Section-5-Middle.png")} alt="" border="0"></img>
+                            <img loading="lazy" className="SPL-LandingPage-Section5-Content-CostSaving3-Logo" src={require("../../../../src/Smart-Platform/Landing Page/Section-5-Right.png")} alt="" border="0"></img>
                         </div>
-                        <div className="div2">
-                            <p className="SPL-LandingPage-Section5-Content-CostSaving1-Title" data-aos="fade-up" data-aos-offset="100">
-                                Cost-saving practices
-                            </p>
-                            <h3 className="SPL-LandingPage-Section5-Content-CostSaving1-Desc" data-aos="fade-up" data-aos-offset="100">
-                                Meningkatkan efisiensi biaya operasional untuk kebutuhan bisnis anda.
-                            </h3>
-                        </div>
-                        <div className="div3">
-                            <p className="SPL-LandingPage-Section5-Content-CostSaving1-Title" data-aos="fade-up" data-aos-offset="100">
-                                Cost-saving practices
-                            </p>
-                            <h3 className="SPL-LandingPage-Section5-Content-CostSaving1-Desc" data-aos="fade-up" data-aos-offset="100">
-                                Meningkatkan efisiensi biaya operasional untuk kebutuhan bisnis anda.
-                            </h3>
+                        <div className="SPL-LandingPage-Section5-Content-CostSaving-Title-Container">
+                            <div>
+                                <p className="SPL-LandingPage-Section5-Content-CostSaving1-Title" data-aos="fade-up" data-aos-offset="100">
+                                    Cost-saving practices
+                                </p>
+                                <h3 className="SPL-LandingPage-Section5-Content-CostSaving1-Desc" data-aos="fade-up" data-aos-offset="100">
+                                    Meningkatkan efisiensi biaya operasional untuk kebutuhan bisnis anda.
+                                </h3>
+                            </div>
+                            <div className="div2">
+                                <p className="SPL-LandingPage-Section5-Content-CostSaving1-Title" data-aos="fade-up" data-aos-offset="100">
+                                    Cost-saving practices
+                                </p>
+                                <h3 className="SPL-LandingPage-Section5-Content-CostSaving1-Desc" data-aos="fade-up" data-aos-offset="100">
+                                    Meningkatkan efisiensi biaya operasional untuk kebutuhan bisnis anda.
+                                </h3>
+                            </div>
+                            <div className="div3">
+                                <p className="SPL-LandingPage-Section5-Content-CostSaving1-Title" data-aos="fade-up" data-aos-offset="100">
+                                    Cost-saving practices
+                                </p>
+                                <h3 className="SPL-LandingPage-Section5-Content-CostSaving1-Desc" data-aos="fade-up" data-aos-offset="100">
+                                    Meningkatkan efisiensi biaya operasional untuk kebutuhan bisnis anda.
+                                </h3>
+                            </div>
                         </div>
                     </div>
                 </div>
