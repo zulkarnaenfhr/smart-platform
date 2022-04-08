3b654e9117db204b5d29b8c0c3e7597ac524e92c
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-04-08 12:56:01 +0700

    SPL LandingPage Section 7 Done Responsive Desktop

--- src/container/1. Smart-Platform/Smart-Platform-LandingPage/Smart-Platform-LandingPage-Section5/Smart-Platform-LandingPage-Section5.jsx
+++ src/container/1. Smart-Platform/Smart-Platform-LandingPage/Smart-Platform-LandingPage-Section5/Smart-Platform-LandingPage-Section5.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { flushSync } from "react-dom";
 import "./Smart-Platform-LandingPage-Section5.css";
 
 class SmartPlatformLandingpageSection5 extends Component {
@@ -10,35 +11,45 @@ class SmartPlatformLandingpageSection5 extends Component {
                         <h1 data-aos="fade-up" data-aos-offset="100">
                             product achievement
                         </h1>
-                        <div className="SPL-LandingPage-Section5-Content-Vector-Container">
-                            <img loading="lazy" className="SPL-LandingPage-Section5-Content-CostSaving-Logo" src={require("../../../../src/Smart-Platform/Landing Page/Section-5-Left.png")} alt="" border="0"></img>
-                            <img loading="lazy" className="SPL-LandingPage-Section5-Content-CostSaving2-Logo" src={require("../../../../src/Smart-Platform/Landing Page/Section-5-Middle.png")} alt="" border="0"></img>
-                            <img loading="lazy" className="SPL-LandingPage-Section5-Content-CostSaving3-Logo" src={require("../../../../src/Smart-Platform/Landing Page/Section-5-Right.png")} alt="" border="0"></img>
-                        </div>
-                        <div className="SPL-LandingPage-Section5-Content-CostSaving-Title-Container">
-                            <div>
-                                <p className="SPL-LandingPage-Section5-Content-CostSaving1-Title" data-aos="fade-up" data-aos-offset="100">
-                                    Cost-saving practices
-                                </p>
-                                <h3 className="SPL-LandingPage-Section5-Content-CostSaving1-Desc" data-aos="fade-up" data-aos-offset="100">
-                                    Meningkatkan efisiensi biaya operasional untuk kebutuhan bisnis anda.
-                                </h3>
+                        <div className="row">
+                            <div className="col-4 SPL-LandingPage-Section5-Content-Container-Section2">
+                                <div>
+                                    <img loading="lazy" className="SPL-LandingPage-Section5-Content-CostSaving-Logo" src={require("../../../../src/Smart-Platform/Landing Page/Section-5-Left.png")} alt="" border="0"></img>
+                                    <div>
+                                        <p className="SPL-LandingPage-Section5-Content-CostSaving1-Title" data-aos="fade-up" data-aos-offset="100">
+                                            Cost-saving practices
+                                        </p>
+                                        <h3 className="SPL-LandingPage-Section5-Content-CostSaving1-Desc" data-aos="fade-up" data-aos-offset="100">
+                                            Meningkatkan efisiensi biaya operasional untuk kebutuhan bisnis anda.
+                                        </h3>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="div2">
-                                <p className="SPL-LandingPage-Section5-Content-CostSaving1-Title" data-aos="fade-up" data-aos-offset="100">
-                                    Cost-saving practices
-                                </p>
-                                <h3 className="SPL-LandingPage-Section5-Content-CostSaving1-Desc" data-aos="fade-up" data-aos-offset="100">
-                                    Meningkatkan efisiensi biaya operasional untuk kebutuhan bisnis anda.
-                                </h3>
+                            <div className="col-4 SPL-LandingPage-Section5-Content-Container-Section2">
+                                <div>
+                                    <img loading="lazy" className="SPL-LandingPage-Section5-Content-CostSaving2-Logo" src={require("../../../../src/Smart-Platform/Landing Page/Section-5-Middle.png")} alt="" border="0"></img>
+                                    <div className="div2">
+                                        <p className="SPL-LandingPage-Section5-Content-CostSaving1-Title" data-aos="fade-up" data-aos-offset="100">
+                                            Cost-saving practices
+                                        </p>
+                                        <h3 className="SPL-LandingPage-Section5-Content-CostSaving1-Desc" data-aos="fade-up" data-aos-offset="100">
+                                            Meningkatkan efisiensi biaya operasional untuk kebutuhan bisnis anda.
+                                        </h3>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="div3">
-                                <p className="SPL-LandingPage-Section5-Content-CostSaving1-Title" data-aos="fade-up" data-aos-offset="100">
-                                    Cost-saving practices
-                                </p>
-                                <h3 className="SPL-LandingPage-Section5-Content-CostSaving1-Desc" data-aos="fade-up" data-aos-offset="100">
-                                    Meningkatkan efisiensi biaya operasional untuk kebutuhan bisnis anda.
-                                </h3>
+                            <div className="col-4 SPL-LandingPage-Section5-Content-Container-Section2">
+                                <div>
+                                    <img loading="lazy" className="SPL-LandingPage-Section5-Content-CostSaving3-Logo" src={require("../../../../src/Smart-Platform/Landing Page/Section-5-Right.png")} alt="" border="0"></img>{" "}
+                                    <div className="div3">
+                                        <p className="SPL-LandingPage-Section5-Content-CostSaving1-Title" data-aos="fade-up" data-aos-offset="100">
+                                            Cost-saving practices
+                                        </p>
+                                        <h3 className="SPL-LandingPage-Section5-Content-CostSaving1-Desc" data-aos="fade-up" data-aos-offset="100">
+                                            Meningkatkan efisiensi biaya operasional untuk kebutuhan bisnis anda.
+                                        </h3>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
--- src/container/1. Smart-Platform/Smart-Platform-LandingPage/Smart-Platform-LandingPage-Section7/Smart-Platform-LandingPage-Section7.jsx
+++ src/container/1. Smart-Platform/Smart-Platform-LandingPage/Smart-Platform-LandingPage-Section7/Smart-Platform-LandingPage-Section7.jsx
@@ -5,12 +5,16 @@ class SmartPlatformLandingpageSection7 extends Component {
     render() {
         return (
             <div id="SPL-LandingPage-Section7">
-                <img className="SPL-LandingPage-Section7-Background" src={require("../../../../src/Smart-Platform/Landing Page/Section-7-Background.png")} alt="" />
-                <h1 data-aos="fade-up" data-aos-offset="100" className="SPL-LandingPage-Section7-Title">
-                    Embark Your AI Journey with Smart platform Now!
-                </h1>
-                <div data-aos="fade-up" data-aos-offset="100">
-                    <button className="SPL-LandingPage-Section7-Contact-Us">Contact Us</button>
+                <div className="SPL-LandingPage-Section7-Container">
+                    <div className="SPL-LandingPage-Section7-Background-Container">
+                        <img className="SPL-LandingPage-Section7-Background" src={require("../../../../src/Smart-Platform/Landing Page/Section-7-Background.png")} alt="" />
+                    </div>
+                    <h1 data-aos="fade-up" data-aos-offset="100" className="SPL-LandingPage-Section7-Title">
+                        Embark Your AI Journey with Smart platform Now!
+                    </h1>
+                    <div data-aos="fade-up" data-aos-offset="100">
+                        <button className="SPL-LandingPage-Section7-Contact-Us">Contact Us</button>
+                    </div>
                 </div>
             </div>
         );
