bd33f695b0cfd4ce03160d8a3145cb6579c29a21
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-04-08 23:03:39 +0700

    SPL-Solution-Section2-Done

--- src/container/1. Smart-Platform/Smart-Platform-Solution/Smart-Platform-Solution-Section2/Smart-Platform-Solution-Section2.jsx
+++ src/container/1. Smart-Platform/Smart-Platform-Solution/Smart-Platform-Solution-Section2/Smart-Platform-Solution-Section2.jsx
@@ -60,7 +60,73 @@ class SmartPlatformSolutionSection2 extends Component {
                         </button>
                     </div>
                     <div className="col-8">
-                        <h1>masuk</h1>
+                        {this.state.statusWatch === "SmartCityGovernance" ? (
+                            <div className="row SPL-Solution-Section2-RightSection">
+                                <div className="col-6">
+                                    <div>
+                                        <img className="SPL-Solution-Section2-RightSection-Logo-FaceDetection" src={require("../../../../src/Smart-Platform/Landing Page/Section-4-Face-Detection.png")} alt="" />
+                                        <p className="SPL-Solution-Section2-RightSection-Title">Face Detection</p>
+                                        <p className="SPL-Solution-Section2-RightSection-Desc">
+                                            Dengan memanfaatkan teknologi 4.0 dalam mengelola dan menganalisa data dengan tepat, bisa menjadi peluang untuk melahirkan sebuah informasi yang tepat guna dan tepat sasaran.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="col-6 SPL-Solution-Section2-RightSection-Col62">
+                                    <div>
+                                        <img className="SPL-Solution-Section2-RightSection-Logo-DocumentDetection" src={require("../../../../src/Smart-Platform/Landing Page/Section-4-Document-Detection.png")} alt="" />
+                                        <p className="SPL-Solution-Section2-RightSection-Title">Document Detection</p>
+                                        <p className="SPL-Solution-Section2-RightSection-Desc">
+                                            Dengan memanfaatkan teknologi 4.0 dalam mengelola dan menganalisa data dengan tepat, bisa menjadi peluang untuk melahirkan sebuah informasi yang tepat guna dan tepat sasaran.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        ) : this.state.statusWatch === "Facility" ? (
+                            <div className="row SPL-Solution-Section2-RightSection">
+                                <div className="col-6">
+                                    <div>
+                                        <img className="SPL-Solution-Section2-RightSection-Logo-FaceDetection" src={require("../../../../src/Smart-Platform/Landing Page/Section-4-Face-Detection.png")} alt="" />
+                                        <p className="SPL-Solution-Section2-RightSection-Title">Face Detection</p>
+                                        <p className="SPL-Solution-Section2-RightSection-Desc">
+                                            Dengan memanfaatkan teknologi 4.0 dalam mengelola dan menganalisa data dengan tepat, bisa menjadi peluang untuk melahirkan sebuah informasi yang tepat guna dan tepat sasaran.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="col-6 SPL-Solution-Section2-RightSection-Col62">
+                                    <div>
+                                        <img className="SPL-Solution-Section2-RightSection-Logo-DataIntegration" src={require("../../../../src/Smart-Platform/Landing Page/Section-4-Data-Integration.png")} alt="" />
+                                        <p className="SPL-Solution-Section2-RightSection-Title">Data Integration</p>
+                                        <p className="SPL-Solution-Section2-RightSection-Desc">
+                                            Dengan memanfaatkan teknologi 4.0 dalam mengelola dan menganalisa data dengan tepat, bisa menjadi peluang untuk melahirkan sebuah informasi yang tepat guna dan tepat sasaran.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        ) : this.state.statusWatch === "Transport" ? (
+                            <div className="row SPL-Solution-Section2-RightSection">
+                                <div className="col-12">
+                                    <div>
+                                        <img className="SPL-Solution-Section2-RightSection-Logo-ObjectDetection" src={require("../../../../src/Smart-Platform/Solution/Smart-Platform-Solution-Section2-ObjectDetection.png")} alt="" />
+                                        <p className="SPL-Solution-Section2-RightSection-Title">Object Detection</p>
+                                        <p className="SPL-Solution-Section2-RightSection-Desc">
+                                            Dengan memanfaatkan teknologi 4.0 dalam mengelola dan menganalisa data dengan tepat, bisa menjadi peluang untuk melahirkan sebuah informasi yang tepat guna dan tepat sasaran.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        ) : (
+                            <div className="row SPL-Solution-Section2-RightSection">
+                                <div className="col-12">
+                                    <div>
+                                        <img className="SPL-Solution-Section2-RightSection-Logo-PaymentBehavior" src={require("../../../../src/Smart-Platform/Landing Page/Section-4-Payment-Behavior.png")} alt="" />
+                                        <p className="SPL-Solution-Section2-RightSection-Title">Payment Behavior</p>
+                                        <p className="SPL-Solution-Section2-RightSection-Desc">
+                                            Dengan memanfaatkan teknologi 4.0 dalam mengelola dan menganalisa data dengan tepat, bisa menjadi peluang untuk melahirkan sebuah informasi yang tepat guna dan tepat sasaran.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
