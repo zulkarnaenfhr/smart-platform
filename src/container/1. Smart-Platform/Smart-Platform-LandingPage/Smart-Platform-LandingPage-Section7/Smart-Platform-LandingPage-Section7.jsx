916864d3103f6d9d3cf9f4e7fe3d6c2b3dd06be4
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-04-08 14:46:59 +0700

    SPL LandingPage Section 7 not Done Responsive Tablet

--- src/container/1. Smart-Platform/Smart-Platform-LandingPage/Smart-Platform-LandingPage-Section7/Smart-Platform-LandingPage-Section7.jsx
+++ src/container/1. Smart-Platform/Smart-Platform-LandingPage/Smart-Platform-LandingPage-Section7/Smart-Platform-LandingPage-Section7.jsx
@@ -9,11 +9,13 @@ class SmartPlatformLandingpageSection7 extends Component {
                     <div className="SPL-LandingPage-Section7-Background-Container">
                         <img className="SPL-LandingPage-Section7-Background" src={require("../../../../src/Smart-Platform/Landing Page/Section-7-Background.png")} alt="" />
                     </div>
-                    <h1 data-aos="fade-up" data-aos-offset="100" className="SPL-LandingPage-Section7-Title">
-                        Embark Your AI Journey with Smart platform Now!
-                    </h1>
-                    <div data-aos="fade-up" data-aos-offset="100">
-                        <button className="SPL-LandingPage-Section7-Contact-Us">Contact Us</button>
+                    <div className="SPL-LandingPage-Section7-Text-Container">
+                        <h1 data-aos="fade-up" data-aos-offset="100" className="SPL-LandingPage-Section7-Title">
+                            Embark Your AI Journey with Smart platform Now!
+                        </h1>
+                        <div data-aos="fade-up" data-aos-offset="100">
+                            <button className="SPL-LandingPage-Section7-Contact-Us">Contact Us</button>
+                        </div>
                     </div>
                 </div>
             </div>
