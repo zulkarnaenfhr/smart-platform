ea0ed6dfd4a74ae01ee1e35f4ba46341f76e9d83
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-04-11 20:14:40 +0700

    BigView Landing Page Section 1 Done

--- src/container/2. BigView/BigView-LandingPage/BigView-LandingPage-Section1/BigView-LandingPage-Section1.jsx
+++ src/container/2. BigView/BigView-LandingPage/BigView-LandingPage-Section1/BigView-LandingPage-Section1.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./BigView-LandingPage-Section1.css";
 
 class BigviewLandingpageSection1 extends Component {
     render() {
         return (
-            <div
-                style={{
-                    height: "100vh",
-                    border: "1px solid red",
-                    background: "black",
-                }}
-            >
-                <h1>bigview section 1</h1>
+            <div id="BigViewLandingPage">
+                <div className="BigViewLandingPage-Background-Container">
+                    <img className="BigViewLandingPage-Section1-Background1" src={require("../../../../src/Big-View/BigView-LandingPage/Background Section 1.png")} alt="" />
+                    <img className="BigViewLandingPage-Section1-Background2" src={require("../../../../src/Big-View/BigView-LandingPage/Background2 Section 1.png")} alt="" />
+                </div>
+                <div className="BigViewLandingPage-Content-Container">
+                    <h1 className="BigViewLandingPage-Section1-Title">
+                        Data <br /> di Dalam <br /> Genggamanmu
+                    </h1>
+                    <h3 className="BigViewLandingPage-Section1-Desc">Lörem ipsum smartplåster plasm köttskatt i bilsurfa.</h3>
+                    <Link
+                        className="BigViewLandingPage-Section1-Button-Container"
+                        to={{
+                            pathname: `notset`,
+                        }}
+                    >
+                        <button className="BigViewLandingPage-Section1-Button">Free Trial</button>
+                    </Link>
+                </div>
             </div>
         );
     }
