255c9fbf5a9902c2dfdec76d3c23bc7cf0a70147
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-04-21 13:35:59 +0700

    Big Score Landing Page Section 2  Done

--- src/container/3. BigScore/BigScore-LandingPage/BigScore-LandingPage-Section2/BigScore-LandingPage-Section2.jsx
+++ src/container/3. BigScore/BigScore-LandingPage/BigScore-LandingPage-Section2/BigScore-LandingPage-Section2.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./BigScore-LandingPage-Section2.css";
 
 class BigscoreLandingpageSection2 extends Component {
     render() {
         return (
-            <div>
-                <h1>msaas</h1>
+            <div id="BigScoreLandingPage-Section2">
+                <div className="BigScoreLandingPage-Section2-container">
+                    <h1 className="BigScoreLandingPage-Section2-Title">About Us</h1>
+                    <img src={require("../../../../src/Big-Score/LandingPage/Section2-Vector.png")} className="BigScoreLandingPage-Section2-Vector" alt="" />
+                    <div className="BigScoreLandingPage-Section2-TextandButton-Container">
+                        <p className="BigScoreLandingPage-Section2-Text">
+                            Big Score is a number that provide information or category of customer’s Telkom Risk Profile that reflects how good or bad a credit risk a particular lender thinks you are
+                        </p>
+                        <Link
+                            className="BigScoreLandingPage-Section2-Button-Container"
+                            to={{
+                                pathname: `notset`,
+                            }}
+                        >
+                            <button className="BigScoreLandingPage-Section2-Button">Learn More</button>
+                        </Link>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/container/3. BigScore/BigScore-LandingPage/BigScoreLandingPage.jsx
+++ src/container/3. BigScore/BigScore-LandingPage/BigScoreLandingPage.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Beforefooterbigview from "../../../components/BeforeFooter/Big View/BeforeFooterBigView";
 import Navbarbigscore from "../../../components/Navbar/Big Score/NavbarBigScore";
 import BigscoreLandingpageSection1 from "./BigScore-LandingPage-Section1/BigScore-LandingPage-Section1";
 import BigscoreLandingpageSection2 from "./BigScore-LandingPage-Section2/BigScore-LandingPage-Section2";
@@ -13,6 +14,7 @@ class Bigscorelandingpage extends Component {
                 </div>
                 <BigscoreLandingpageSection1 />
                 <BigscoreLandingpageSection2 />
+                <Beforefooterbigview />
             </div>
         );
     }
