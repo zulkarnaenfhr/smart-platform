ba83702eb302ca9c74457d8f3b7133247b738611
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-04-22 16:22:43 +0700

    section 1 done responsive desktop

--- src/container/3. BigScore/BigScore-AboutUs/BigScoreAboutUs.jsx
+++ src/container/3. BigScore/BigScore-AboutUs/BigScoreAboutUs.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Beforefooterbigscore from "../../../components/BeforeFooter/Big Score/BeforeFooterBigScore";
+import Footerbigscore from "../../../components/Footer/Big-Score/FooterBigScore";
 import Navbarbigscore from "../../../components/Navbar/Big Score/NavbarBigScore";
 import BigscoreAboutusSection1 from "./BigScore-AboutUs-Section1/BigScore-AboutUs-Section1";
 import BigscoreAboutusSection2 from "./BigScore-AboutUs-Section2/BigScore-AboutUs-Section2";
@@ -17,6 +19,8 @@ class Bigscoreaboutus extends Component {
                 <BigscoreAboutusSection2 />
                 <BigscoreAboutusSection3 />
                 <BigscoreAboutusSection4 />
+                <Beforefooterbigscore />
+                <Footerbigscore />
             </div>
         );
     }
