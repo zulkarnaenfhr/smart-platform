4bdad754666d030cbc95ae312ec4cdbc4186168e
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-04-22 13:06:53 +0700

    section 7 responsive desktop

--- src/container/3. BigScore/BigScore-LandingPage/BigScore-LandingPage-Section7/BigScore-LandingPage-Section7.jsx
+++ src/container/3. BigScore/BigScore-LandingPage/BigScore-LandingPage-Section7/BigScore-LandingPage-Section7.jsx
@@ -7,13 +7,15 @@ class BigscoreLandingpageSection7 extends Component {
             <div id="BigScoreLandingPage-Section7">
                 <div className="BigScoreLandingPage-Section7-Content-Container">
                     <h1 className="BigScoreLandingPage-Section7-Title">How Big Score Work</h1>
-                    <img src={require("../../../../src/Big-Score/LandingPage/Section7-Vector1.png")} className="BigScoreLandingPage-Section7-Vector1" alt="" />
-                    <img src={require("../../../../src/Big-Score/LandingPage/Section7-Panah1.png")} className="BigScoreLandingPage-Section7-Panah1" alt="" />
-                    <img src={require("../../../../src/Big-Score/LandingPage/Section7-Vector2.png")} className="BigScoreLandingPage-Section7-Vector2" alt="" />
-                    <img src={require("../../../../src/Big-Score/LandingPage/Section7-Vector3.png")} className="BigScoreLandingPage-Section7-Vector3" alt="" />
-                    <img src={require("../../../../src/Big-Score/LandingPage/Section7-Vector4.png")} className="BigScoreLandingPage-Section7-Vector4" alt="" />
-                    <img src={require("../../../../src/Big-Score/LandingPage/Section7-Panah1.png")} className="BigScoreLandingPage-Section7-Panah2" alt="" />
-                    <img src={require("../../../../src/Big-Score/LandingPage/Section7-Vector5.png")} className="BigScoreLandingPage-Section7-Vector5" alt="" />
+                    <div className="BigScoreLandingPage-Section7-Vector-Container">
+                        <img src={require("../../../../src/Big-Score/LandingPage/Section7-Vector1.png")} className="BigScoreLandingPage-Section7-Vector1" alt="" />
+                        <img src={require("../../../../src/Big-Score/LandingPage/Section7-Panah1.png")} className="BigScoreLandingPage-Section7-Panah1" alt="" />
+                        <img src={require("../../../../src/Big-Score/LandingPage/Section7-Vector2.png")} className="BigScoreLandingPage-Section7-Vector2" alt="" />
+                        <img src={require("../../../../src/Big-Score/LandingPage/Section7-Vector3.png")} className="BigScoreLandingPage-Section7-Vector3" alt="" />
+                        <img src={require("../../../../src/Big-Score/LandingPage/Section7-Vector4.png")} className="BigScoreLandingPage-Section7-Vector4" alt="" />
+                        <img src={require("../../../../src/Big-Score/LandingPage/Section7-Panah1.png")} className="BigScoreLandingPage-Section7-Panah2" alt="" />
+                        <img src={require("../../../../src/Big-Score/LandingPage/Section7-Vector5.png")} className="BigScoreLandingPage-Section7-Vector5" alt="" />
+                    </div>
                 </div>{" "}
             </div>
         );
